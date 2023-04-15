95cc0d57405cb3a7fe011109ba35119090ba18a0
Alexander Liu <a@alexanderliu.com>
2023-04-15 02:11:44 -0700

    feat: select quote time range

--- frontend/src/components/Quotes/Quotes.tsx
+++ frontend/src/components/Quotes/Quotes.tsx
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 
 import useSWR from "swr";
 import Quote from "./Quote";
 
 import styles from "./Quotes.module.css";
 
+type Quote = {
+  name: string;
+  message: string;
+  time: string;
+};
+
+const calculateOffset = (days: number) => {
+  const date = new Date();
+  const offsetDate = new Date();
+  offsetDate.setDate(date.getDate() + days);
+  return offsetDate.toISOString().slice(0, -1);
+};
+
 const fetcher = (...args: Parameters<typeof fetch>) =>
   fetch(...args).then((res) => res.json());
 
 const Quotes = () => {
-  type Quote = {
-    name: string;
-    message: string;
-    time: string;
-  };
+  const [after, setAfter] = useState<number>(7);
+  const searchParams = useMemo(
+    () => (after < 0 ? "" : "after=" + calculateOffset(-after)),
+    [after]
+  );
+
   const { data, error, isLoading } = useSWR<Quote[]>(
-    `http://localhost:8000/quotes?after=2022-11-03T20:23:54`,
+    `${import.meta.env.VITE_API}/quotes?${searchParams}`,
     fetcher
   );
 
   return (
     <>
-      <h2 className={styles.heading}>Previous Quotes</h2>
+      <div className={styles.info}>
+        <h2 className={styles.heading}>Previous Quotes</h2>
+        <select
+          value={after}
+          onChange={(e) => setAfter(parseInt(e.target.value))}
+        >
+          <option value={1}>Last Day</option>
+          <option value={7}>Last Week</option>
+          <option value={30}>Last Month</option>
+          <option value={-1}>All Time</option>
+        </select>
+      </div>
       <div className={styles.quotes}>
         {data
           // Sort messages in decending order.
